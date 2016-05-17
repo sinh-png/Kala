3447840f31f956531157cb0449f9bccf6017c5b6
hazame <hazagames404@gmail.com>
2016-05-17 22:15:13 +0700

    AssetsBuilder: Build files array for Assets.

--- kala/internal/AssetsBuilder.hx
+++ kala/internal/AssetsBuilder.hx
@@ -7,41 +7,59 @@ import sys.io.File;
 
 class AssetsBuilder {
 	
+	public static var resPath(default, null):String;
+	public static var resJson(default, null):Dynamic;
+	public static var files(default, null):Array<Dynamic>;
+	
 	public static inline function findResources():String {
 		return kha.internal.AssetsBuilder.findResources();
 	}
 
 	macro public static function build(type:String):Array<Field> {
 		var fields = Context.getBuildFields();
-		var path = findResources();
-		var content = Json.parse(File.getContent(path + "files.json"));
-		var files:Iterable<Dynamic> = content.files;
 		
-		path = path.substr(0, path.length - 11) + '/';
+		if (resPath == null) {
+			resPath = findResources();
+			resJson = Json.parse(File.getContent(resPath + "files.json"));
+			files = resJson.files;
+		}
+
+		var path = resPath.substr(0, resPath.length - 11) + '/';
 		
-		for (file in files) {
-			var name:String = file.name;
+		switch(type) {
 			
-			switch(type) {
+			case "FileArray":
+				fields.push({
+					name: "files",
+					doc: "The info of the files listed in files.json.",
+					meta: [],
+					access: [APublic, AStatic],
+					kind: FProp("default", "never", macro :Array<Dynamic>, macro $v{files}),
+					pos: Context.currentPos()
+				});
+			
+			case "Sheet":
+				var json:Dynamic;
 				
-				case "sheet":
-					var ext = "_ssd";
+				for (file in files) {
+					var name:String = file.name;
 					
+					var ext = "_ssd";
+				
 					if (file.type != "blob" || name.lastIndexOf(ext, name.length - ext.length) == -1) continue;
 					
 					name = name.substr(0, name.length - ext.length);
-					content = Json.parse(File.getContent(path + file.files[0]));
+					json = Json.parse(File.getContent(path + file.files[0]));
 					
 					fields.push({
 						name: name,
 						doc: null,
 						meta: [],
-						access: [APublic],
-						kind: FVar(macro :SheetData, macro new SheetData($v{content.frames})),
+						access: [APublic, AStatic],
+						kind: FProp("default", "never", macro :SheetData, macro new SheetData($v{json.frames})),
 						pos: Context.currentPos()
 					});
-					
-			}
+				}
 		}
 
 		return fields;
