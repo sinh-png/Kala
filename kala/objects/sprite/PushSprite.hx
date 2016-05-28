6a0a32578c10d596e705128a7550557b78382cbf
hazame <hazagames404@gmail.com>
2016-05-29 06:39:36 +0700

    Add PushSprite.

---
+++ kala/objects/sprite/PushSprite.hx
@@ -0,0 +1,161 @@
+package kala.objects.sprite;
+
+#if (kala_mouse || kala_touch)
+
+import kala.components.collision.Collider;
+import kala.components.collision.CollisionShape;
+import kala.EventHandle.CallbackHandle;
+import kala.math.Vec2;
+import kha.FastFloat;
+import kha.Image;
+
+/**
+ * Sprite with basic touch and / or mouse interaction using Collider component. 
+ * Good for making basic button.
+ */
+class PushSprite extends Sprite {
+
+	public var collider(default, null):Collider;
+	
+	public var hovered(default, null):Bool;
+
+	/**
+	 * The second arg is id of touch or mouse button (1 - left, 2 - middle, 3 - right).
+	 */
+	public var onPush(default, null):CallbackHandle<PushSprite->Int->Void>;
+	/**
+	 * The second arg is id of touch or mouse button (1 - left, 2 - middle, 3 - right).
+	 */
+	public var onRelease(default, null):CallbackHandle<PushSprite->Int->Void>;
+	public var onOver(default, null):CallbackHandle<PushSprite->Void>;
+	public var onOut(default, null):CallbackHandle<PushSprite->Void>;
+	
+	private var _mouseHovered:Bool;
+	private var _touched:Bool;
+	
+	public function new(
+		?image:Image, 
+		?frameX:Int, ?frameY:Int, 
+		?frameWidth:Int, ?frameHeight:Int,
+		animated:Bool = false
+	) {
+		super(image, frameX, frameY, frameWidth, frameHeight, animated);
+		
+		this.collider = new Collider(this);
+		
+		onPush = addCBHandle(new CallbackHandle<PushSprite->Int->Void>());
+		onRelease = addCBHandle(new CallbackHandle<PushSprite->Int->Void>());
+		onOver = addCBHandle(new CallbackHandle<PushSprite->Void>());
+		onOut = addCBHandle(new CallbackHandle<PushSprite->Void>());
+	}
+	
+	override public function reset(componentsReset:Bool = false):Void {
+		super.reset(componentsReset);
+		hovered = _mouseHovered = _touched = false;
+	}
+	
+	override public function destroy(destroyComponents:Bool = true):Void {
+		super.destroy(destroyComponents);
+		
+		collider = null;
+		
+		onPush = null;
+		onRelease = null;
+		onOver = null;
+		onOut = null;
+	}
+	
+	override public function update(elapsed:FastFloat):Void {
+		#if kala_mouse
+		updateMouse();
+		#end
+		
+		#if kala_touch
+		updateTouch();
+		#end
+		
+		if (hovered && !_touched && !_mouseHovered) {
+			for (callback in onOut) callback.cbFunction(this);
+			hovered = false;
+		}
+	}
+	
+	public inline function addCircleMask(x:FastFloat, y:FastFloat, radius:FastFloat):CollisionCircle {
+		return collider.addCircle(x, y, radius);
+	}
+	
+	public inline function addRectMask(x:FastFloat, y:FastFloat, width:FastFloat, height:FastFloat):CollisionPolygon {
+		return collider.addRect(x, y, width, height);
+	}
+	
+	public inline function addPolygonMask(x:FastFloat, y:FastFloat, vertices:Array<Vec2>, concave:Bool = false):Array<CollisionPolygon> {
+		return collider.addPolygon(x, y, vertices, concave);
+	}
+	
+	public inline function addObjectRectMask():CollisionPolygon {
+		return collider.addObjectRect();
+	}
+	
+	public inline function addShapeMask(shape:CollisionShape):CollisionShape {
+		return collider.addShape(shape);
+	}
+	
+	#if kala_mouse
+	function updateMouse():Void {
+		if (collider.testPoint(kala.input.Mouse.x, kala.input.Mouse.y)) {
+			if (!hovered) {
+				hovered = true;
+				for (callback in onOver) callback.cbFunction(this);
+			}
+			
+			if (kala.input.Mouse.LEFT.justPressed) {
+				for (callback in onPush) callback.cbFunction(this, 1);
+			} else if (kala.input.Mouse.LEFT.justReleased) {
+				for (callback in onRelease) callback.cbFunction(this, 1);
+			}
+			
+			if (kala.input.Mouse.MIDDLE.justPressed) {
+				for (callback in onPush) callback.cbFunction(this, 2);
+			} else if (kala.input.Mouse.MIDDLE.justReleased) {
+				for (callback in onRelease) callback.cbFunction(this, 2);
+			}
+			
+			if (kala.input.Mouse.RIGHT.justPressed) {
+				for (callback in onPush) callback.cbFunction(this, 3);
+			} else if (kala.input.Mouse.RIGHT.justReleased) {
+				for (callback in onRelease) callback.cbFunction(this, 3);
+			}
+			
+			_mouseHovered = true;
+		} else {
+			_mouseHovered = false;
+		}
+	}
+	#end
+	
+	#if kala_touch
+	function updateTouch():Void {
+		_touched = false;
+		
+		for (touch in kala.input.Touch.touches) {
+			if (collider.testPoint(touch.x, touch.y)) {
+				if (!hovered) {
+					hovered = true;
+					for (callback in onOver) callback.cbFunction(this);
+				}
+				
+				if (touch.justStarted) {
+					for (callback in onPush) callback.cbFunction(this, touch.id);
+				} else if (touch.justEnded) {
+					for (callback in onRelease) callback.cbFunction(this, touch.id);
+				}
+				
+				_touched = true;
+			}
+		}
+	}
+	#end
+	
+}
+
+#end
