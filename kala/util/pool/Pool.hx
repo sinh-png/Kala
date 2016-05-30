6ec5e2d89355e19b2a1bd5a735a1ea9029129202
hazame <hazagames404@gmail.com>
2016-05-30 13:28:29 +0700

    Delete ObjectPool. Move pool module to util. Remove initFunction. Update put() and add putUnsafe().

---
+++ kala/util/pool/Pool.hx
@@ -0,0 +1,45 @@
+package kala.util.pool;
+
+class Pool<T> {
+
+	public var factoryFunction:Void->T;
+
+	private var _objects:Array<T> = new Array<T>();
+	
+	public function new(?factoryFunction:Void->T) {
+		this.factoryFunction = factoryFunction;
+	}
+	
+	public function destroy():Void {
+		factoryFunction = null;
+		_objects = null;
+	}
+	
+	public function get():T {
+		var obj:T = null;
+		
+		if (_objects.length > 0) {
+			obj = _objects.pop();
+		} else {
+			if (factoryFunction != null) {
+				obj = factoryFunction();
+			}
+		}
+		
+		return obj;
+	}
+	
+	/**
+	 * Put an object into the pool regardless if the object is already in the pool or not.
+	 */
+	public inline function putUnsafe(obj:T):Void {
+		if (obj != null) _objects.push(obj);
+	}
+	
+	public inline function put(obj:T):Void {
+		if (obj != null && _objects.indexOf(obj) == -1) {
+			_objects.push(obj);
+		}
+	}
+	
+}
