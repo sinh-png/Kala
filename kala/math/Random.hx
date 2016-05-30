ef9986a557ce29bf540a7c4d7aa55787808bfe46
hazame <hazagames404@gmail.com>
2016-05-30 13:29:33 +0700

    Add math.Random.

---
+++ kala/math/Random.hx
@@ -0,0 +1,40 @@
+package kala.math;
+
+import kha.FastFloat;
+
+abstract Random(kha.math.Random) from kha.math.Random to kha.math.Random {
+
+	public static var instance(default, never):Random = new Random(6172039);
+	
+	public static inline function int(min:Int, max:Int):Int {
+		return instance.getInt(min, max);
+	}
+	
+	public static inline function float(min:Float, max:Float):Float {
+		return instance.getFloat(min, max);
+	}
+	
+	public static inline function fast(min:FastFloat, max:FastFloat):FastFloat {
+		return instance.getFast(min, max);
+	}
+	
+	//
+	
+
+	public inline function new(seed:Int) {
+		this = new kha.math.Random(seed);
+	}
+	
+	public inline function getInt(min:Int, max:Int):Int {
+		return this.GetIn(min, max);
+	}
+	
+	public inline function getFloat(min:Float, max:Float):Float {
+		return this.GetFloatIn(min, max);
+	}
+	
+	public inline function getFast(min:FastFloat, max:FastFloat):FastFloat {
+		return this.GetFloatIn(min, max);
+	}
+	
+}
